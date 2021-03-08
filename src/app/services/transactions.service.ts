22ce6901f58026994b24188bc6da2428f8743691
Isaac Levi Tzab Poot <isaac.tzab@mother.software>
2021-03-08 09:50:27 -0600

    enable api with jsonServer

--- src/app/services/transactions.service.ts
+++ src/app/services/transactions.service.ts
@@ -1,27 +1,44 @@
 import { Injectable } from '@angular/core';
 import { ITransaction } from '../interface/transaction';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+// import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Observable, of } from 'rxjs';
+
+import * as API from '../../mock/transactions.json';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TransactionsService {
-  private API = '/api/transactions';
-  httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-  };
-  constructor(
-    private http: HttpClient,
-  ) {}
 
-  getTransactions(): Observable <ITransaction[]> {
-    return this.http.get<ITransaction[]>(this.API);
+// export class TransactionsService {
+//   private API = '/api/transactions';
+//   httpOptions = {
+//     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+//   };
+//   constructor(
+//     private http: HttpClient,
+//   ) {}
+
+//   getTransactions(): Observable <ITransaction[]> {
+//     return this.http.get<ITransaction[]>(this.API);
+//   }
+//   updateTransactions(transaction: ITransaction): Observable<any> {
+//     return this.http.put(this.API, transaction, this.httpOptions);
+//   }
+//   addTransactions(transaction: ITransaction): Observable<ITransaction> {
+//     return this.http.post<ITransaction>(this.API, transaction, this.httpOptions);
+//   }
+// }
+
+export class TransactionsService {
+  data: ITransaction[] = API.data as ITransaction[];
+  constructor() {
+    console.log(this.data);
   }
-  updateTransactions(transaction: ITransaction): Observable<any> {
-    return this.http.put(this.API, transaction, this.httpOptions);
+  getTransactions(): Observable<ITransaction[]> {
+    return of(this.data);
   }
   addTransactions(transaction: ITransaction): Observable<ITransaction> {
-    return this.http.post<ITransaction>(this.API, transaction, this.httpOptions);
+    this.data.unshift(transaction);
+    return of();
   }
 }
